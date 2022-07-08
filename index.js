71a80a73850c430ac5e51d7dacf997ded40df4db
Maria Coitinho <brasildu@gmail.com>
2022-07-08 17:10:37 -0500

    commit tree a8710ce4bff60d87de7054e2329a69a126efc8f4

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+  const superbowlWin = function(array) {
+    let winObject = array.find(element => {
+    return element.result === "W";
+  })
+    // console.log(winObject)
+  if (winObject === undefined) {
+    return undefined;
+  } else {
+    return winObject.year;
+  }
+  }
+console.log(superbowlWin(record));
